9bfc42d510295f9affca0287bd3829aa8da64568
gregjw <me@gregjw.com>
2014-09-10 10:37:42 +0100

    v1.2
    Added ‘main.css’ to add some styling.
    Also reformatted ‘index.php’ to make it a lot more user-friendly.

--- index.php
+++ index.php
@@ -38,21 +38,22 @@ function getUserID($userName){
 }
 
 function printImages($userID){
-	$url = 'https://api.instagram.com/v1/users/'. $userID .'/media/recent?client_id='. clientID .'&count=5';
+	$url = 'https://api.instagram.com/v1/users/'. $userID .'/media/recent?client_id='. clientID .'&count=-1';
 	$instagramInfo = connectToInstagram($url);
 	$results = json_decode($instagramInfo, true);
 	
+    echo '<head><title>instagramDL - DONE </title><link href="main.css" rel="stylesheet" type="text/css"></head><br /><div id="dl"><p align="center"> Your Instagram photos can now be saved! </p></div> <br/><br/>';
+    
 	foreach($results['data'] as $item){
 		$image_url = $item['images']['low_resolution']['url'];
-		echo '<img src="'.$image_url.'" /> <br/>';
+		echo '<p align="center"><img src="'.$image_url.'" /> </p><br/>';
 		savePicture($image_url);
 	}
 }
 
 function savePicture($image_url){
-	echo $image_url . '<br />';
 	$filename = basename($image_url);
-	echo $filename . '<br />';
+	
 	
 	$destination = imageDirectory.$filename;
 	file_put_contents($destination, file_get_contents($image_url));
@@ -87,8 +88,24 @@ if($_GET['code']){
 
 <!DOCTYPE html>
 <html>
+<head>
+<title>instagramDL</title>
+<link href="main.css" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel=
+    'stylesheet' type='text/css'>
+</head>
 <body>
-	<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">Login</a>
+<div id="dl" align="center">
+	<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">Log in to Instagram</a>
+    <br>
+    <br>
+    <p> It's 100% Safe, I promise.</p>
+    <p> This website works in tandem with the Instagram API, so when you click that button, it'll take you to Instagram. <br>
+    This website never touches your data. We just receive your photos so you can save them easier!</p>
+    <br>
+    <br>
+    <a href="http://gregjw.com">Back to gregjw.com</a>
+</div>
 </body>
 </html>
 
